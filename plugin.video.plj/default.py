31e5fd4249c9e189bbc9bc39fd68988dee08f13d
panglangjang <panglangjang@gmail.com>
2013-04-06 00:31:30 -0400

    Bump 0.0.2 (Skin + Addon)
    + added missing localization for Stream Menu
    + plugin.video.plj:
    - pull icons from plugins
    - embed reliability rating into titles

--- plugin.video.plj/default.py
+++ plugin.video.plj/default.py
@@ -9,31 +9,31 @@
     :license: GPLv3, see LICENSE.txt for more details.
 '''
 
-from xbmcswift2 import actions
+#from xbmcswift2 import actions
 import urllib,urllib2,re,xbmcplugin,xbmcgui
 
 def CATEGORIES():
-        categories = [
-                ( 'International', [
-                        ( '网络电视(PPS)', 'plugin://plugin.video.ppstream/' )
-                ] ),
-
-                ( 'Local', [
-                        ( '香港看(HKKan)','plugin://plugin.video.hkkan/' ),
-                        ( '迅雷看看(KanKan)', 'plugin//plugin.video.kankan/' ),
-                        ( '音悦台(YingYueTai)','plugin://plugin.video.yingyuetai/' ),
-
-                ] )
-
+        plugins = [
+                        ( 95, '香港看(HKKan)','plugin.video.hkkan', 'hkk.png'),
+                        ( 80, '迅雷看看(KanKan)','plugin.video.kankan', 'kk.png' ),
+                        ( 70, '网络电视(PPS)', 'plugin.video.ppstream','pps.png' ),
+                        ( 90, '音悦台(YinYueTai)','plugin.video.yinyuetai','yyt.png' )
                 ]
-        for category,addons in categories:
-                addDir('- '+category,'',0,'')
-                for name, url in addons:
-                        addDir(name,url,1,'')
+        for rating, name, url, icon in plugins:
+                addPlugin('可靠性: '+str(rating)+'% | '+ str(name), 'plugin://'+str(url),'special://home/addons/'+str(url)+'/icon.png')
 
 def BRIDGE(url):
-        actions.update_view(url)
-                
+        #actions.update_view(url)
+        xbmc.executebuiltin('Container.Update('+url+')')
+
+def addPlugin(name,url,iconimage):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode=1"
+        ok=True
+        liz=xbmcgui.ListItem(name,iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz)
+        return ok
+                        
 def get_params():
         param=[]
         paramstring=sys.argv[2]
@@ -51,15 +51,6 @@ def get_params():
                                 param[splitparams[0]]=splitparams[1]
                                 
         return param
-
-def addDir(name,url,mode,iconimage):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-        ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name } )
-        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
-        return ok
-        
               
 params=get_params()
 url=None
