b80d2e4f90f5d88407d7907879b998801ac3d861
panglangjang <panglangjang@gmail.com>
2013-04-09 10:36:45 -0400

    HKkan 1.5.0 - Beautiful Soup

--- plugin.video.hkkan/default.py
+++ plugin.video.hkkan/default.py
@@ -8,59 +8,78 @@
         GPLv3
 
 '''
+from bs4 import BeautifulSoup
 from t0mm0.common.net import Net
 import urllib,urllib2,re,xbmcplugin,xbmcgui
 
-__home__    =   None
-__category__=   1
-__show__    =   2
+__category__    =   None
+__shows__       =   1
+__episodes__    =   2
 
 #def __init__(self):
 
-def HOME():
-        #addDir('Search','http://www.khmeravenue.com/',4,'http://yeuphim.net/images/logo.png')
-        addDir('新的东西','http://azdrama.net/recently-updated',__category__,'')
-        #addDir('English Subtitles','http://azdrama.net/english/&sort=date',7,'')
-        addDir('电视剧','http://azdrama.net/hk-drama',__category__,'')
-        addDir('综艺','http://azdrama.net/hk-show',__category__,'')
-        addDir('韩国 电视剧','http://azdrama.net/korean-drama',__category__,'')
-        #addDir('Mainland Chinese Dramas','http://azdrama.net/chinese-drama/',__category__,'')
-        #addDir('Taiwanese Dramas','http://azdrama.net/taiwanese-drama/',2,'')
+def Paginate(url,page):
+    return url + "/page-" + str(page) + ".html"
 
-def INDEX(url):
+def CATEGORIES():
+    addDir('新的东西','http://azdrama.net/recently-updated',__shows__,'')
+    addDir('电视剧','http://azdrama.net/hk-drama',__shows__,'')
+    addDir('综艺','http://azdrama.net/hk-show',__shows__,'')
+    addDir('韩国 电视剧','http://azdrama.net/korean-drama',__shows__,'')
+    #addDir('Mainland Chinese Dramas','http://azdrama.net/chinese-drama/',__shows__,'')
+    #addDir('Taiwanese Dramas','http://azdrama.net/taiwanese-drama/',2,'')
+    #addDir('English Subtitles','http://azdrama.net/english/&sort=date',7,'')
+    #addDir('Search','http://www.khmeravenue.com/',4,'http://yeuphim.net/images/logo.png')
+
+
+def SHOWS(url,page):
     #try:
-        link = GetContent(url)
-        newlink = ''.join(link.splitlines()).replace('\t','')
-        listcontent=re.compile('<div class="content">(.+?)<div id="r">').findall(newlink)
-        match=re.compile('<img [^>]*src=["\']?([^>^"^\']+)["\']?[^>]*></a><h1 class="normal"><a href="(.+?)" title="(.+?)">(.+?)</a><span class="download">').findall(listcontent[0])
-        for (vimg,vurl,vname,vtmp) in match:
-            vurl = vurl +'list-episode/'
-            print vurl
+        html = GetContent(Paginate(url,page))
+        html = ''.join(html.splitlines()).replace('\t','')
+
+        soup = BeautifulSoup(html)
+
+        #print soup.prettify("utf-8")
+        
+        content = soup.find("div","content")
+        pages = soup.find("div","wp-pagenavi")
+
+        #debug initial scrape
+        #print content.get_text()
+        #print pages.get_text()
+
+        for link in content.find_all("a",recursive=False):
+            #print link
+            title = link["title"]
+            href = link["href"] + "list-episode/"
+            img = link.find("img")["src"]
             try:
-                  addDir(vname,vurl,__show__,vimg)
+                addDir(title,href,__episodes__,img)
             except:
-                  #if the addDir fails, its probably because some args need to be encoded to utf-8, because they contain chinese characters.
-                  vimg = vimg.encode('utf-8')
-                  vurl = vurl.encode('utf-8')
-                  vname = vname.encode('utf-8')
-                  addDir(vname,vurl,__show__,vimg)
-        pagecontent=re.compile('<div class="wp-pagenavi" align=center>(.+?)</div>').findall(newlink)
-        if(len(pagecontent)>0):
-                match5=re.compile('<a href="(.+?)" class="(.+?)" title="(.+?)">(.+?)</a>').findall(pagecontent[0])
+                addDir(title.encode('utf-8'),href.encode('utf-8'),__episodes__,img.encode('utf-8'))               
 
-                #removing last element doesnt always handle the problem.
-                #you get &laquo; if you are not on the first page
-                #del match5[-1]
-                for vurl,vtmp,vname,vtmp2 in match5:
-                    #so far, this greedy replace call will have to suffice
-                    #Note: the paginated pages do follow a pattern
-                    #ex: http://azdrama.net/recently-updated/page-2.html
-                    #in other words, this plugins menu can be written alot better.
-                    vname = vname.replace('&laquo;','<<')
-                    vname = vname.replace('&raquo;','>>')
-                    addDir(vname,vurl,__category__,"")
-    #except: pass
+        #create page directories, by finding the last available page link.
+        AddPagesDir(url,pages,__shows__)
 
+def AddPagesDir(url,pages,mode):
+    
+    if(pages):
+        current = int(pages.find("span","current").get_text())
+        links = pages.find_all("a",["page","last"],recursive=False)
+        
+        last_page = int(re.compile("page-([\d]+?)\.html").findall(str(links[-1]))[0])
+        if (current > last_page): #when on the last page, there is no link for it.
+            last_page = current
+        
+        print "There are " + str(last_page) + " Pages for this category, currently on Page " + str(current)
+        page = 1
+        while(page <= last_page):
+            name = "Page " + str(page)
+            if page == current:
+                name += " (Currently Viewing)"
+                
+            addDir(name, url, mode,"",page)
+            page += 1    
 
 def GetContent(url):
     try:
@@ -71,12 +90,19 @@ def GetContent(url):
        d = xbmcgui.Dialog()
        d.ok(url,"不能得到的东西",'请再试一次')
 	
-def Episodes(url,name,newmode):
-    link = GetContent(url)
-    newlink = ''.join(link.splitlines()).replace('\t','')
-    listcontent=re.compile('<ul class="listep">(.+?)</ul>').findall(newlink)
+def EPISODES(url,name,page=1):
+    html = GetContent(Paginate(url,page))
+    html = ''.join(html.splitlines()).replace('\t','')
 
-    match=re.compile('<li><a href="(.+?)" title="[^"]+ (?:Episode [0]*([\d]+)|([\d]{4}-[\d]{2}-[\d]{2}))">').findall(listcontent[0])
+    soup = BeautifulSoup(html)
+
+
+    content = soup.find("div","content")
+    #link = GetContent(Paginate(url,page))
+    #newlink = ''.join(link.splitlines()).replace('\t','')
+    #listcontent=re.compile('<ul class="listep">(.+?)</ul>').findall(newlink)
+
+    match=re.compile('<li><a href="(.+?)" title="[^"]+ (?:Episode [0]*([\d]+)|([\d]{4}-[\d]{2}-[\d]{2}))">').findall(str(content))
 
     is_mirror = len(match[0][1])==0 & len(match[0][2])==0
     #listlink = re.compile('<a href="([^\"]+?)">Watch all episodes</a>').findall(newlink)
@@ -94,10 +120,11 @@ def Episodes(url,name,newmode):
         match=re.compile('<a href="(.+?)"><font [^>]*><b>[^0-9]*([0-9]+?)[^<]*</b></font></a>').findall(listcontent[0])        
 
     else: #must be all hosted
+
         print "Processing Hosted Episodes: "
         #print listlink
         #request full episodes list:
-
+        '''
         #scrap through all pages if paginated
         last_page= re.compile('<a href="'+url+'page-([\d]+)\.html" class="last"').findall(newlink)
         if(len(last_page)>0):
@@ -116,9 +143,7 @@ def Episodes(url,name,newmode):
                 match+=re.compile('<li><a href="(.+?)" title="[^"]+ (?:Episode [0]*([\d]+)|([\d]{4}-[\d]{2}-[\d]{2}))">').findall(listcontent[0])
 
                 page_num+=1
-        
-        
-        
+        '''        
         match.reverse() #default order is descending
 
     print "Listing Episodes"
@@ -151,6 +176,10 @@ def Episodes(url,name,newmode):
                 #should append to array of all cached links, so it can be loaded later without so much scraping"
                 addLink(name + " - 第" + str(order),urllib.unquote(vlink[0]),'')
 
+    pages = soup.find("div","wp-pagenavi")
+    #print pages.prettify("utf-8")
+    AddPagesDir(url,pages,__episodes__)
+    
 
 def addLink(name,url,iconimage):
         ok=True
@@ -159,10 +188,10 @@ def addLink(name,url,iconimage):
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
         return ok
 
-def addDir(name,url,mode,iconimage):
-        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+def addDir(name,url,mode,iconimage,page=1):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&page="+str(page)
         ok=True
-        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage=iconimage)
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
         liz.setInfo( type="Video", infoLabels={ "Title": name } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
@@ -191,6 +220,7 @@ params=get_params()
 url=None
 name=None
 mode=None
+page=None
 try:
         url=urllib.unquote_plus(params["url"])
 except:
@@ -203,14 +233,19 @@ try:
         mode=int(params["mode"])
 except:
         pass
+try:
+        page=int(params["page"])
+except:
+        pass
+
 
 sysarg=str(sys.argv[1])
 print "mode is:" + str(mode)
-if mode==__home__ or url==None or len(url)<1:
-        HOME()
-elif mode==__category__:
-        INDEX(url)
-elif mode==__show__:
-        Episodes(url,name,mode)
+if mode==__category__ or url==None or len(url)<1:
+        CATEGORIES()
+elif mode==__shows__:
+        SHOWS(url,page)
+elif mode==__episodes__:
+        EPISODES(url,name,page)
 
 xbmcplugin.endOfDirectory(int(sysarg))
